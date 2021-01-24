2d5c05c54cf591facab0982fe0dd203d53d9d9a7
Oskar Sveinsen <osvein@users.noreply.github.com>
2021-01-24 20:30:36 +0100

    grow vec

--- util.h
+++ util.h
@@ -8,18 +8,18 @@
 #define lenof(a) (sizeof(a)/sizeof(*(a)))
 #define endof(a) (a + lenof(a))
 
-typedef struct { short x, y; } Vec;
+typedef struct { int32_t x, y; } Vec;
 
 static const Vec vec_zero = {0, 0};
 
-/* returns integer square root of x, capped at 2^bits
+/* returns integer (floored) square root of x, capped at 2^bits
  * fast, gcc tends to unroll with constant bits at -O3
  */
-static inline unsigned short isqrt(unsigned long x, unsigned bits) {
-	unsigned y = 0;
+static inline int32_t isqrt(int64_t x, unsigned bits) {
+	uint32_t y = 0;
 
 	while (bits-- > 0) {
-		unsigned a = (y + (1 << bits)) << bits;
+		uint64_t a = (y + (1 << bits)) << bits;
 
 		if (x >= a) {
 			x -= a;
@@ -32,12 +32,12 @@ static inline unsigned short isqrt(unsigned long x, unsigned bits) {
 /* returns the norm (length) squared of vector v
  * this is faster than vec_norm because it avoids sqrt
  */
-static inline unsigned long vec_normsq(Vec v) {
-	return (unsigned long)v.x * v.x + (unsigned long)v.y * v.y;
+static inline int64_t vec_normsq(Vec v) {
+	return (int64_t)v.x * v.x + (int64_t)v.y * v.y;
 }
 
 /* returns the norm (length) of vector v */
-static inline unsigned short vec_norm(Vec v) {
+static inline int32_t vec_norm(Vec v) {
 	return isqrt(vec_normsq(v), CHAR_BIT*sizeof(v.x));
 }
 
@@ -49,8 +49,11 @@ static inline Vec vec_add(Vec a, Vec b) {
 /* scales vector v by the fraction num/denom
  * overflow-unsafe except when |denom|>=|num|
  */
-static inline Vec vec_scale(Vec v, short num, long denom) {
-	return (Vec){((long)v.x * num) / denom, ((long)v.y * num) / denom};
+static inline Vec vec_scale(Vec v, int32_t num, int64_t denom) {
+	return (Vec){
+		(int32_t)(((int64_t)v.x * num) / denom),
+		(int32_t)(((int64_t)v.y * num) / denom)
+	};
 }
 
 /* negates vector v */
@@ -59,17 +62,17 @@ static inline Vec vec_neg(Vec v) {
 }
 
 /* returns a vector with same angle as v, and specified magnitude */
-static inline Vec vec_normalize(Vec v, short magnitude) {
+static inline Vec vec_normalize(Vec v, int32_t magnitude) {
 	return vec_scale(v, magnitude, vec_norm(v));
 }
 
 /* returns a random vector inside a rectangle using rand() */
-static inline Vec vec_rand(Vec min, Vec max) {
+/*static inline Vec vec_rand(Vec min, Vec max) {
 	return (Vec){
-		rand() % ((long)max.x + 1) + min.x,
-		rand() % ((long)max.y + 1) + min.y
+		rand() % ((int64_t)max.x + 1) + min.x,
+		rand() % ((int64_t)max.y + 1) + min.y
 	};
-}
+}*/
 
 static inline Vec vec_min(Vec a, Vec b) {
 	return (Vec){a.x < b.x ? a.x : b.x, a.y < b.y ? a.y : b.y};
